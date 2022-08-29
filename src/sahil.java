443c8c27c2e14b2a48d01961381601c2bb4e3d64
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-08-29 12:23:24 +0530

    floyds tringle

--- src/sahil.java
+++ src/sahil.java
@@ -3,12 +3,13 @@ import java.util.*;
 public class sahil {
     public static void main(String args[]) {
         int n=5;
-        for(int i=n; i>=1; i--){
+        int number=1;
+        for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
-                System.out.print(j+" ");
+                System.out.print(number+" ");
+                number++;
             }
             System.out.println();
         }
-
     }
 }
