b792c17a83c4b208cdd099546973a144b25c0dd3
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-08-30 18:30:21 +0530

    edit input and output 0,1 tringle

--- src/sahil.java
+++ src/sahil.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class sahil {
     public static void main(String args[]) {
-        int n=5;
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
                 int sum=i+j;
