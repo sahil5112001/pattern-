aa9911943eb7335768588ff26e90737fc9eae9a8
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-08-30 18:25:23 +0530

    0 or 1 tringle

--- src/sahil.java
+++ src/sahil.java
@@ -3,13 +3,18 @@ import java.util.*;
 public class sahil {
     public static void main(String args[]) {
         int n=5;
-        int number=1;
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
-                System.out.print(number+" ");
-                number++;
+                int sum=i+j;
+                if(sum%2==0){
+                    System.out.print("1"+" ");
+                }
+                else{
+                    System.out.print("0"+" ");
+                }
             }
-            System.out.println();
+            System.out.println( );
+
         }
     }
 }
