46e43ab84992358782ec8307bd71486db9f25665
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-09-03 12:31:17 +0530

    solid rhombus

--- src/sahil.java
+++ src/sahil.java
@@ -2,20 +2,16 @@ import java.util.*;
 
 public class sahil {
     public static void main(String args[]) {
-        Scanner sc= new Scanner(System.in);
-        int n= sc.nextInt();
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                int sum=i+j;
-                if(sum%2==0){
-                    System.out.print("1"+" ");
-                }
-                else{
-                    System.out.print("0"+" ");
-                }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
             }
-            System.out.println( );
-
+            for (int j = 1; j <= n; j++) {
+                    System.out.print("*");
+                }
+            System.out.println();
         }
     }
 }
