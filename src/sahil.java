bddca79c37729ba327f7790a6ad20fb9b0b1e589
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-09-22 16:40:05 +0530

    function to add two no.and return sum

--- src/sahil.java
+++ src/sahil.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
 public class sahil {
-    public static void printMyName(String name){
-        System.out.println(name);
-        return;
+    public static int calculateSum(int a, int b){
+        int sum = a+b;
+        return sum;
     }
-    public static void main(String args[]) {
+    public static void main(String args[])  {
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        printMyName(name);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum=calculateSum(a,b);
+        System.out.println("sum of two no.:" +sum);
         }
-
     }
