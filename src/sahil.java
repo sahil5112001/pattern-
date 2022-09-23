3c40a12b75bf404db0550f2898da5d5e00a87f0a
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-09-23 18:01:41 +0530

    print table with function

--- src/sahil.java
+++ src/sahil.java
@@ -1,28 +1,14 @@
 import java.util.*;
-
 public class sahil {
-//    public static int calculateProduct(int a, int b){
-//         int product = a*b;
-//         return product;
-//    }
-    public static void printfactorial(int n){
-        if (n < 0){
-            System.out.println("invalid numbaer");
-                    return;
+    public static void printCalculateTable(int n) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(n * i);
         }
-        int factorial = 1;
-        for (int i = n; i>=1; i--) {
-            factorial = factorial * i;
-        }
-
-            System.out.println(factorial);
-            return;
-        }
-
-    public static void main(String args[])  {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        printfactorial(n);
+      return;
+    }
+        public static void main (String args[]){
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            printCalculateTable(n);
         }
     }
