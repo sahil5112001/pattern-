46f55c8416e7a8bcce774b3294ac753cafc04c5d
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-09-23 11:34:33 +0530

    find factorial of a number

--- src/sahil.java
+++ src/sahil.java
@@ -1,15 +1,28 @@
 import java.util.*;
 
 public class sahil {
-    public static int calculateProduct(int a, int b){
-         int product = a*b;
-         return product;
-    }
+//    public static int calculateProduct(int a, int b){
+//         int product = a*b;
+//         return product;
+//    }
+    public static void printfactorial(int n){
+        if (n < 0){
+            System.out.println("invalid numbaer");
+                    return;
+        }
+        int factorial = 1;
+        for (int i = n; i>=1; i--) {
+            factorial = factorial * i;
+        }
+
+            System.out.println(factorial);
+            return;
+        }
+
     public static void main(String args[])  {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int product=calculateProduct(a,b);
-        System.out.println("product of two no.:" +product);
+        int n = sc.nextInt();
+
+        printfactorial(n);
         }
     }
