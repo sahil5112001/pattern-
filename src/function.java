e4a91fc496fe2aeceb7e91e972863d6a9f42d4d6
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-09-25 18:06:26 +0530

    print table with function

---
+++ src/function.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class function {
+
+    public static void printCalculateTable(int n) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println(n * i);
+        }
+        return;
+    }
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printCalculateTable(n);
+    }
+}
