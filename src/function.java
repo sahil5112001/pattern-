f4dd5f7c2012914174c1d68466fe169fbf52b0dd
Sshil Hardaha <sahilhardaha2000@gmail.com>
2022-09-26 18:33:04 +0530

    function to print sum of all odd no.till n

--- src/function.java
+++ src/function.java
@@ -1,18 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class function {
 
-    public static int printCalculateAverage(int a,int b,int c) {
-        int average = (a+b+c)/3;
+    public static int printCalculateSum(int n) {
+       int sum=0 ;
+       for(int i=0; i<=n; i++){
+           if(i%2!=0){
+               sum=sum+i;
+           }
+       }
+       System.out.println(sum);
 
-        return average;
+        return sum;
     }
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int Average = printCalculateAverage(a,b,c);
-        System.out.println(Average);
+        int n = sc.nextInt();
+         printCalculateSum(n);
     }
 }
--- src/sahil.java
+++ src/sahil.java
@@ -1,14 +1,14 @@
 import java.util.*;
 public class sahil {
-    public  static void printCalculateTable(int n) {
-        for (int i = 1; i < 11; i++) {
-            System.out.println(n * i);
-        }
-      return;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum = sum + i;
+            }
+        }                System.out.println(sum);
+
     }
-        public static void main (String args[]){
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            printCalculateTable(n);
-        }
-    }
+}
